8ea813157a2f166f289003071c2e5f9dfebcf475
LibXZR <i@xzr.moe>
2022-08-21 18:37:53 +0800

    Launcher3: Add support for parallel space
    spkal01: Forward Port to 13
    Change-Id: I13bee37cae645f50512f4d80cdfa25f84dce1b6b
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/allapps/WorkProfileManager.java
+++ src/com/android/launcher3/allapps/WorkProfileManager.java
@@ -84,7 +84,7 @@ public class WorkProfileManager implements PersonalWorkSlidingTabStrip.OnActiveP
 
     private final UserManager mUserManager;
     private final ActivityAllAppsContainerView<?> mAllApps;
-    private final Predicate<ItemInfo> mMatcher;
+    private Predicate<ItemInfo> mMatcher;
     private final StatsLogManager mStatsLogManager;
 
     private WorkModeSwitch mWorkModeSwitch;
@@ -99,7 +99,6 @@ public class WorkProfileManager implements PersonalWorkSlidingTabStrip.OnActiveP
             StatsLogManager statsLogManager) {
         mUserManager = userManager;
         mAllApps = allApps;
-        mMatcher = mAllApps.mPersonalMatcher.negate();
         mStatsLogManager = statsLogManager;
     }
 
@@ -214,6 +213,10 @@ public class WorkProfileManager implements PersonalWorkSlidingTabStrip.OnActiveP
 
     public Predicate<ItemInfo> getMatcher() {
         return mMatcher;
+    }
+
+    public void updateMatcher() {
+        mMatcher = mAllApps.mPersonalMatcher.negate();
     }
 
     @Nullable
