d5e4a534a2ac40aab5ecbb4b5476fe1f98d1d174
StarkDroid <gamerprince.exp@gmail.com>
2023-02-03 13:30:09 +0800

    Launcher3: Recents: Add preference to enable scroll vibration
    Change-Id: Id65a5bd93f04a9e458ff9c510326e6d7fa1c86f0
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -182,6 +182,7 @@ public final class Utilities {
     public static final String KEY_SINGLE_PAGE_CENTER = "pref_single_page_center";
     public static final String KEY_DRAWER_SEARCH = "pref_drawer_search";
     public static final String KEY_TRANSIENT_TASKBAR = "pref_transient_taskbar";
+    public static final String KEY_VIBRATION_TOGGLE = "pref_recents_vibration";
 
     /**
      * Returns true if theme is dark.
@@ -944,4 +945,9 @@ public final class Utilities {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_TRANSIENT_TASKBAR, false);
     }
+
+    public static boolean recentsScrollVibration(Context context) {
+    	SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+    	return prefs.getBoolean(KEY_VIBRATION_TOGGLE, true);
+   }
 }
