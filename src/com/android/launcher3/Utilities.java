97ec2861ec254d72b1dc8fa9b887211f26f060c2
minaripenguin <minaripenguin@users.noreply.github.com>
2023-06-11 23:00:43 +0800

    Launcher3: Expose transient taskbar settings
    Change-Id: If86826152bd79eb563563dd24f2110e8518b234e
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -181,6 +181,7 @@ public final class Utilities {
     public static final String KEY_SHORT_PARALLAX = "pref_short_parallax";
     public static final String KEY_SINGLE_PAGE_CENTER = "pref_single_page_center";
     public static final String KEY_DRAWER_SEARCH = "pref_drawer_search";
+    public static final String KEY_TRANSIENT_TASKBAR = "pref_transient_taskbar";
 
     /**
      * Returns true if theme is dark.
@@ -937,5 +938,10 @@ public final class Utilities {
     public static boolean showSearch(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DRAWER_SEARCH, true);
+    }
+
+    public static boolean isTransientTaskBarEnabled(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_TRANSIENT_TASKBAR, false);
     }
 }
